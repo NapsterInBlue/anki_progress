48ccfff43d857de6076603d40f7aad37d9148f79
NapsterInBlue <NapsterInBlue@gmail.com>
2020-07-17 15:36:51 -0400

    Minute spent studying and accumulation of cards done

--- data.py
+++ data.py
@@ -1,13 +1,26 @@
+"""
+Utilities for extracting and cleaning our Anki data
+
+This README of schema definitions was super helpful in
+constructing these functions
+
+    https://github.com/ankidroid/Anki-Android/wiki/Database-Structure
+"""
+
+import sqlite3
+from contextlib import contextmanager
+from datetime import datetime, timedelta
+from itertools import zip_longest
+from typing import Dict, List
+
 import pandas as pd
 
-
-from contextlib import contextmanager
-import sqlite3
+DB_PATH = "C:/Users/Nick/AppData/Roaming/Anki2/Nick/"
 
 
 @contextmanager
 def connect_to_db(database: str):
-    path = "C:/Users/Nick/AppData/Roaming/Anki2/Nick/"
+    path = DB_PATH
     conn = sqlite3.connect(path + database)
     cur = conn.cursor()
 
@@ -17,7 +30,14 @@ def connect_to_db(database: str):
     conn.close()
 
 
-def get_study_time():
+def get_study_time() -> pd.DataFrame:
+    """
+    Get the number of seconds spent studying per day.
+
+    Gets the min date and the max date from the data and
+    fills with 0s where appropriate.
+    """
+
     sql = """
         SELECT
         strftime('%Y-%m-%d', datetime(round(id / 1000), 'unixepoch', 'localtime')) as review_date,
@@ -56,3 +76,92 @@ def get_study_time():
 
     return merged
 
+
+def get_reviewed_cards() -> List[int]:
+    """
+    Query all cards that appear in the review log
+    table and also have a valid card id (not deleted)
+    """
+    sql = """
+        select
+            distinct r.cid
+            from    revlog r
+                left join  cards c
+                    on     c.id = r.cid
+            where   r.ivl > 0
+            and  c.id is not null
+    """
+
+    with connect_to_db("collection.anki2") as cursor:
+        cursor.execute(sql)
+        reviewed_cards = [x[0] for x in cursor.fetchall()]
+
+    return reviewed_cards
+
+
+def get_card_status_by_day(card_id: int) -> pd.DataFrame:
+    """
+    From the first day we've reviewed it to today, what status
+    ('known', 'due') was each card in?
+    """
+    sql = f"""
+        select
+            cid,
+            strftime('%Y-%m-%d', datetime(id / 1000, 'unixepoch', 'localtime')),
+            ivl
+
+            from    revlog
+            where   cid = {card_id}
+                and ivl > 0
+    """
+    card_statuses = dict()
+
+    with connect_to_db("collection.anki2") as cursor:
+        cursor.execute(sql)
+        data = cursor.fetchall()
+
+        # look at pairs of days until there are no more,
+        # fill with today when we run out of values
+        row_a = [
+            (datetime(*map(int, date.split("-"))), delta) for _, date, delta in data
+        ]
+        row_b = row_a[1:]
+
+        for pair in zip_longest(row_a, row_b, fillvalue=(datetime.now(), 0)):
+            (review_date, delta), (next_review_date, _) = pair
+
+            due_date = review_date + timedelta(days=delta)
+
+            days_to_next_review = (next_review_date - review_date).days
+            for day in range(days_to_next_review):
+                date = review_date + timedelta(days=day)
+                date_str = datetime.strftime(date, "%Y-%m-%d")
+
+                past_due = date > (due_date)
+
+                if past_due:
+                    card_statuses[date_str] = "due"
+                else:
+                    card_statuses[date_str] = "known"
+
+    df = pd.DataFrame(
+        {
+            "card_id": card_id,
+            "date": list(card_statuses.keys()),
+            "status": list(card_statuses.values()),
+        }
+    )
+
+    return df
+
+
+def get_all_card_statuses_by_day() -> pd.DataFrame:
+    reviewed_cards = get_reviewed_cards()
+
+    dfs = []
+    for card_id in reviewed_cards:
+        dfs.append(get_card_status_by_day(card_id))
+
+    df = pd.concat(dfs).reset_index(drop=True)
+
+    return df
--- viz.py
+++ viz.py
@@ -10,6 +10,12 @@ def plot_trended_study_time(df):
     loc = matplotlib.ticker.MultipleLocator(250)
     ax.xaxis.set_major_locator(loc)
 
+    loc = matplotlib.ticker.MultipleLocator(60 * 60)
+    ax.yaxis.set_major_locator(loc)
+
+    loc = matplotlib.ticker.MultipleLocator(30 * 60)
+    ax.yaxis.set_minor_locator(loc)
+
     def minutes(x, pos):
         return "{:,.1f}".format(x / 60)
 
@@ -29,6 +35,24 @@ def plot_trended_study_time(df):
         label="30 day rolling avg",
     )
 
-    ax.legend(loc="upper left")
+    ax.legend(loc="upper left", fontsize=12)
+    ax.grid(True, "major", axis="y", c="k")
+    ax.grid(True, "minor", axis="y", alpha=0.5)
+
+    ax.set_title("Minutes Spent Studying", fontsize=20)
+
+    return ax
+
+
+def plot_card_catalog_growth(df):
+    known_cards = df.query('status=="known"')["date"].value_counts().sort_index()
+    all_cards = df["date"].value_counts().sort_index()
+
+    fig, ax = plt.subplots(figsize=(16, 10))
+    known_cards.plot(ax=ax, c="b", label="Known Cards")
+    all_cards.plot(ax=ax, c="r", label="Total Cards Seen")
+
+    ax.legend(loc="upper left", fontsize=12)
+    ax.set_title("Accumulation of Cards", fontsize=20)
 
     return ax
